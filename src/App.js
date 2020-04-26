ab838c557aec4dc12a19b2bd22c9eaabf6fdb91b
dunapanta <dunapanta2@gmail.com>
2020-04-25 20:49:48 -0500

    Agregando Estilos

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Amplify from 'aws-amplify';
-import aws_exports from './aws-exports';
-import { withAuthenticator } from 'aws-amplify-react';
-import '@aws-amplify/ui/dist/style.css';
+import RegistrationPage from './views/Auth/Registration';
 
-Amplify.configure(aws_exports);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RegistrationPage />
   );
 }
-export default withAuthenticator(App, true);
+
+export default App;
