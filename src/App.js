9030af01cefbe46d16f2ce63223885938d02ad26
dunapanta <dunapanta2@gmail.com>
2020-04-26 23:31:12 -0500

    version obtenida de youtube funcionando

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import RegistrationPage from './views/Auth/Registration';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import SignUpForm from './views/Auth/Registration';
+import SignInForm from './views/Auth/Login';
+Amplify.configure(aws_exports);
 
-function App() {
-  return (
-    <RegistrationPage />
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            signedUp : false
+        }
+        this.handleSignup = this.handleSignup.bind(this);
+    }
+
+    handleSignup() {
+        this.setState({
+            signedUp: true
+        });
+    }
+    render() {
+        const { signedUp } = this.state;
+        return !signedUp ? <SignUpForm handleSignup={ this.handleSignup }/> : <SignInForm />;
+    }
 }
 
 export default App;
--- src/views/Auth/Login.js
+++ src/views/Auth/Login.js
@@ -1,154 +1,85 @@
-import React from "react";
-// @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import Icon from "@material-ui/core/Icon";
-// @material-ui/icons
-import Email from "@material-ui/icons/Email";
-import People from "@material-ui/icons/People";
-// core components
-import Header from "components/Header/Header.js";
-import HeaderLinks from "components/Header/HeaderLinks.js";
-import Footer from "components/Footer/Footer.js";
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
-import Button from "components/CustomButtons/Button.js";
-import Card from "components/Card/Card.js";
-import CardBody from "components/Card/CardBody.js";
-import CardHeader from "components/Card/CardHeader.js";
-import CardFooter from "components/Card/CardFooter.js";
-import CustomInput from "components/CustomInput/CustomInput.js";
+import React, { Component } from 'react';
+import { Auth } from 'aws-amplify';
 
-import styles from "assets/jss/material-kit-react/views/loginPage.js";
+class SignInForm extends Component {
+    constructor(props) {
+        super(props);
+  
+        this.state = {
+            user: '',
+            password: '',
+            signedIn: false
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.signIn = this.signIn.bind(this);
+        this.confirmSignIn = this.confirmSignIn.bind(this);
+    }
+  
+    signIn() {
+        const { username, password } = this.state;  
+        Auth.signIn({
+            username: username,
+            password: password
+        })
+        .then(() => console.log('successfully signed in'))
+        .catch((err) => console.log(`Error signing in: ${ err }`))
+    }
+  
+    confirmSignIn() {
+        const { username } = this.state;
+        Auth.confirmSignIn(username)
+        .then(() => console.log('successfully confirmed signed in'))
+        .catch((err) => console.log(`Error confirming sign up - ${ err }`))
+    }
+  
+    handleSubmit(e) {
+        e.preventDefault();
 
-import image from "assets/img/bg7.jpg";
-
-const useStyles = makeStyles(styles);
-
-export default function LoginPage(props) {
-  const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
-  setTimeout(function() {
-    setCardAnimation("");
-  }, 700);
-  const classes = useStyles();
-  const { ...rest } = props;
-  return (
-    <div>
-      {/* <Header
-        absolute
-        color="transparent"
-        brand="Material Kit React"
-        rightLinks={<HeaderLinks />}
-        {...rest}
-      /> */}
-      <div
-        className={classes.pageHeader}
-        style={{
-          backgroundImage: "url(" + image + ")",
-          backgroundSize: "cover",
-          backgroundPosition: "top center"
-        }}
-      >
-        <div className={classes.container}>
-          <GridContainer justify="center">
-            <GridItem xs={12} sm={12} md={4}>
-              <Card className={classes[cardAnimaton]}>
-                <form className={classes.form}>
-                  <CardHeader color="primary" className={classes.cardHeader}>
-                    <h4>Login</h4>
-                    <div className={classes.socialLine}>
-                      <Button
-                        justIcon
-                        href="#pablo"
-                        target="_blank"
-                        color="transparent"
-                        onClick={e => e.preventDefault()}
-                      >
-                        <i className={"fab fa-twitter"} />
-                      </Button>
-                      <Button
-                        justIcon
-                        href="#pablo"
-                        target="_blank"
-                        color="transparent"
-                        onClick={e => e.preventDefault()}
-                      >
-                        <i className={"fab fa-facebook"} />
-                      </Button>
-                      <Button
-                        justIcon
-                        href="#pablo"
-                        target="_blank"
-                        color="transparent"
-                        onClick={e => e.preventDefault()}
-                      >
-                        <i className={"fab fa-google-plus-g"} />
-                      </Button>
-                    </div>
-                  </CardHeader>
-                  <p className={classes.divider}>Iniciar Sesión</p>
-                  <CardBody>
-                    {/* <CustomInput
-                      labelText="Nombre Usuario"
-                      id="first"
-                      formControlProps={{
-                        fullWidth: true
-                      }}
-                      inputProps={{
-                        type: "text",
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            <People className={classes.inputIconsColor} />
-                          </InputAdornment>
-                        )
-                      }}
-                    /> */}
-                    <CustomInput
-                      labelText="Email..."
-                      id="email"
-                      formControlProps={{
-                        fullWidth: true
-                      }}
-                      inputProps={{
-                        type: "email",
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            <Email className={classes.inputIconsColor} />
-                          </InputAdornment>
-                        )
-                      }}
-                    />
-                    <CustomInput
-                      labelText="Contraseña"
-                      id="pass"
-                      formControlProps={{
-                        fullWidth: true
-                      }}
-                      inputProps={{
-                        type: "password",
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            <Icon className={classes.inputIconsColor}>
-                              lock_outline
-                            </Icon>
-                          </InputAdornment>
-                        ),
-                        autoComplete: "off"
-                      }}
-                    />
-                  </CardBody>
-                  <CardFooter className={classes.cardFooter}>
-                    <Button simple color="primary" size="lg">
-                      ENTRAR
-                    </Button>
-                  </CardFooter>
-                </form>
-              </Card>
-            </GridItem>
-          </GridContainer>
-        </div>
-        {/* <Footer whiteFont /> */}
-      </div>
-    </div>
-  );
+        this.signIn();
+        this.confirmSignIn()
+        this.setState({
+            username: '',
+            password: '',
+            signedIn: true
+        });
+        e.target.reset();
+    }
+  
+    handleChange(e) {
+        if (e.target.id === 'username') {
+          this.setState({
+              username: e.target.value
+          });
+        } else if (e.target.id === 'password') {
+          this.setState({
+              password: e.target.value
+          });
+        }
+    }
+  
+    render() {
+      const { signedIn } = this.state;
+      if (signedIn) {
+          return (
+              <div>
+                  <h1>You have signed in!</h1>
+              </div>
+          );
+      } else {
+        return (
+          <div>
+            <form onSubmit={ this.handleSubmit }>
+                <label>Username</label>
+                <input id='username' type='text' onChange={ this.handleChange }/>
+                <label>Password</label>
+                <input id='password' type='password' onChange={ this.handleChange }/>
+                <button>Sign In</button>
+            </form>
+          </div>
+        );
+      }
+    }
 }
+
+export default SignInForm;
